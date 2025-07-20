be5ddd30c9d748a45e809a8a05557cfb09541505
Kraust <secretdragoon@gmail.com>
2025-07-19 20:09:57 -0400

    Updated to auto-session.

--- lua/user/event.lua
+++ lua/user/event.lua
@@ -3,6 +3,13 @@ local definitions = {
 	bufs = {
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
 	},
+	wins = {
+		{
+			"VimEnter",
+			"*",
+			[[nested if !argc() && filereadable(expand('~/.config/nvim/session.vim')) | source ~/.config/nvim/session.vim |]],
+		},
+	},
 }
 
 return definitions
